15bf582e2ff8e17bb89a01a3bb2824522f477216
unknown <desirejogbeh@gmail.com>
2023-11-21 12:01:40 +0100

    part of signup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Head from "next/head";
-import { JobOpening } from "@/components/JobOpening";
-import { InfoBox } from "@/components/InfoBox";
+import { Nunito } from 'next/font/google';
+
+const nunitobold = Nunito({
+  subsets:['latin'],
+  weight:'700'
+});
+
+const nunito = Nunito({
+  subsets:['latin'],
+});
+
 
 export default function index() {
   return (
@@ -8,18 +17,34 @@ export default function index() {
       <Head>
         <title>Agrotrade</title>
       </Head>
-      <main>
-        <InfoBox>
-          <ul>
-            <li>Watermelon</li>
-            <li>orange</li>
-            <li>guava</li>
-          </ul>
-        </InfoBox>
-        <JobOpening title='Next JS Hybrid Work' location='Nairobi'/>
+      <main className="bg-home w-full h-screen flex flex-col justify-end md:w-full h-screen lg:flex flex-row justify-end">
+        {/* bottom content */}
+        <div className="green-box flex flex-col justify-end gap-6 h-[40%] bg-gradient-to-b from-green-600/90 to-green-900 p-3 pb-12 md:flex flex-col justify-center">
 
-        <JobOpening title='React Developer' location='Enugu'/>
+          <div className={styles.block}>
+            <h1 className={`${nunitobold.className} text-5xl`}>
+              <span className="text-white">Agro</span>
+              <span className="text-lime-300">Trade</span>
+            </h1>
+
+            <p className="text-center text-md text-white">
+              Your easy and reliable online marketplace
+              to trade farm produce
+            </p>
+          </div>
+
+          <blockquote className={styles.block}>
+            <button className={`${styles.btn} bg-lime-500`}>Login</button>
+            <button className={`${styles.btn} bg-white`}>Register</button>
+          </blockquote>
+
+        </div>
       </main>
     </>
   )
+}
+
+const styles = {
+  block: 'flex flex-col items-center gap-2',
+  btn: 'w-full h-[48px] flex justify-center items-center text-xl font-bold rounded-xl'
 }
