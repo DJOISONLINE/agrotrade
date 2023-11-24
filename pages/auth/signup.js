861f5052a40b980481c9a8e9b666859c69039414
unknown <desirejogbeh@gmail.com>
2023-11-24 18:06:13 +0100

    Navbar and footer

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { TextField } from "@mui/material";
+import { FcGoogle } from "react-icons/fc";
+import { FaApple } from "react-icons/fa";
 
 export default function signup() {
+    const [tab,setTab] = useState('buyer');
     return (
         <>
         <Head>
             <title>signup | Agrotrade</title>
         </Head>
-        <main className="h-screen flex justify-center items-center py-20">
+        <main className="h-screen flex justify-center items-center py-20 px-3 md:px-0">
             <div className="w-full md:w-[420px] flex flex-col gap-3 border border-gray-300 rounded-md p-3">
                 <Image 
                 width={400} 
@@ -16,15 +20,19 @@ export default function signup() {
                 className="rounded-t-md"
                 src="/farm-trade.jpg" alt="farm trade"/>
 
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-5 md:gap-3">
                     <ul className="grid grid-cols-2">
-                        <li className="text-center">Farmer</li>
-                        <li className="text-center">Buyer</li>
+                        <li 
+                        className={`text-center font-bold pb-3 cursor-pointer ${tab == 'farmer' ? styles.tabColor : null}`}
+                        onClick={() => setTab('farmer')}>Farmer</li>
+                        <li 
+                        className={`text-center font-bold pb-3 cursor-pointer ${tab == 'buyer' ? styles.tabColor : null}`}
+                        onClick={() => setTab('buyer')}>Buyer</li>
                     </ul>
 
-                    <div>
-                        <h2 className="text-2xl">Register as a buyer account</h2>
-                        <p className="text-xs text-green-600">Fill the form below to create a buyer's account</p>
+                    <div className="fex flex-col gap-2">
+                        <h2 className="text-2xl">{`Register as a ${tab} account`}</h2>
+                        <p className="text-xs text-green-600">{`Fill the form below to create a ${tab}'s account`}</p>
                     </div>
 
                     <form>
@@ -40,13 +48,41 @@ export default function signup() {
                             <TextField className="w-full" type="email" variant="outlined" label="email"/>
                         </div>
 
+                        {
+                            tab == 'farmer' ?
+                                <div className="mb-2">
+                                <TextField className="w-full" variant="outlined" label="company name"/>
+                            </div> : null
+                        }
+
                         <button className="h-[48px] w-full flex justify-center items-center bg-green-700 hover:bg-green-800
                          text-white text-xl
                         rounded-md">Register</button>
                     </form>
+
+                    {/* OR seperator */}
+                    <div className="separator"> <span className='text-gray-400 my-3'>OR</span> </div>
+
+
+                    {/* social signup */}
+                    <div className="flex flex-col gap-3">
+
+                        <button 
+                        className="h-[48px] flex justify-center items-center border border-slate-400 rounde-md text-slate-900">
+                            <FcGoogle className="text-3xl mr-2"/>Signup with Google</button>
+
+                            <button 
+                        className="h-[48px] flex justify-center items-center border border-slate-400 rounde-md text-slate-900">
+                            <FaApple className="text-3xl mr-2"/>Signup with Apple</button>
+
+                    </div>
                 </div>
             </div>
         </main>
         </>
     )
+}
+
+const styles = {
+    tabColor: "border-b-4 border-green-600"
 }
